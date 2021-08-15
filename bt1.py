f0afd40bba2590be5afdf057717ece868c649c5f
son857764 <88228857+son857764@users.noreply.github.com>
2021-08-15 14:54:18 +0700

    ​Viết chương trình tìm tất cả các số chia hết cho 7 nhưng không phải bội số của 5, nằm trong đoạn 2000 và 3200 (tính cả 2000 và 3200). Các số thu được sẽ được in thành chuỗi trên một dòng, cách nhau bằng dấu phẩy.

---
+++ bt1.py
@@ -0,0 +1,11 @@
+#Câu hỏi : Viết chương trình tìm tất cả các số chia hết cho 7
+#  nhưng không phải bội số của 5, nằm trong đoạn 2000 và 3200
+#  (tính cả 2000 và 3200). Các số thu được sẽ được in thành 
+# chuỗi trên một dòng, cách nhau bằng dấu phẩy.
+
+list = []
+
+for i in range (2000,3201):
+    if i % 7 == 0 and i % 5 != 0:
+        list.append(str(i))
+print (','.join(list))
