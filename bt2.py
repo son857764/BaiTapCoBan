b848ad567c9205d4d0e940e68f31f1f36d4a049c
son857764 <88228857+son857764@users.noreply.github.com>
2021-08-15 14:58:30 +0700

    Viết một chương trình có thể tính giai thừa của một số cho trước. Kết quả được in thành chuỗi trên một dòng, phân tách bởi dấu phẩy. Ví dụ, số cho trước là 8 thì kết quả đầu ra phải là 40320.

---
+++ bt2.py
@@ -0,0 +1,13 @@
+#Câu hỏi : Viết một chương trình có thể tính giai thừa của 
+# một số cho trước. Kết quả được in thành chuỗi trên một dòng, 
+# phân tách bởi dấu phẩy. Ví dụ, số cho trước là 8 thì kết
+#  quả đầu ra phải là 40320.
+
+n = int(input("Số cần tính giai thừa n = "))
+def fact():
+    if n == 0:
+        return 1
+    return n * ( n - 1 )
+
+print("Giai thừa của",n,"là:" ,fact())
+        
