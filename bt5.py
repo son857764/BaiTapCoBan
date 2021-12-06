21dd2932b487daea48ef4494a00a7d0c837e937b
son857764 <88228857+son857764@users.noreply.github.com>
2021-12-06 15:52:58 +0700

    Định dạng print in ra

---
+++ bt5.py
@@ -0,0 +1,4 @@
+print('%10s %19s' % ('muoi_ki_tu','day la')) #thay s là string = d là số int
+=> muoi_ki_tu              day la
+print("{:<8} {:<24} {:<8} {:<8}{:<8} {:<8} {:<8} {:<8} {:<8}"
+              .format("ID", "Name", "Sex", "Age", "Toan", "Ly", "Hoa", "Diem TB", "Hoc Luc")) #thay < bằng >
